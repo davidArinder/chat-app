f88d9e310494efe59831b6d65135b78d4a20562d
David Arinder <david.arinder@gmail.com>
2019-07-07 11:37:19 -0700

    added share location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,4 @@
 // client side
-
 const socket = io() // connect to server
 
 socket.on('message', (message) => {
@@ -12,4 +11,17 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     const message = e.target.elements.message.value
     
     socket.emit('sendMessage', message)
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude, 
+            longitude: position.coords.longitude
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -16,10 +16,19 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('message', 'Welcome!')
+    socket.emit('message', 'Welcome!') // send to single user
+    socket.broadcast.emit('message', 'A new user has joined') // send to everyone except user
 
     socket.on('sendMessage', (message) => {
-        io.emit('message', message)
+        io.emit('message', message) // send to all users
+    })
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
     })
 })
 
