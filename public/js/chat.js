67fc85306b571735c731748c5424d7537fe802e8
David Arinder <david.arinder@gmail.com>
2019-07-07 12:22:35 -0700

    added acknowledgements, button functionality

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,27 +1,51 @@
 // client side
 const socket = io() // connect to server
 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on('message', (message) => {
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
-    e.preventDefault()
+// send messages
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault() // prevent page refresh
     
+    $messageFormButton.setAttribute('disabled', 'disabled') // disables form once it's been submitted
+
     const message = e.target.elements.message.value
     
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled') // enables form after acknowledgement callback
+        $messageFormInput.value = '' // clear input after message sent
+        $messageFormInput.focus() // move focus back to input after message sent
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered') // acknowledgement for user
+    })
 })
 
-document.querySelector('#send-location').addEventListener('click', () => {
+// share location
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
     }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled') // disables send location button after submit
 
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude, 
             longitude: position.coords.longitude
+        }, () => {
+            $sendLocationButton.removeAttribute('disabled') // enables send location after acknowledgement callback
+            console.log('Location shared') // acknowledge location shared
         })
     })
 })
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
@@ -19,12 +20,22 @@ io.on('connection', (socket) => {
     socket.emit('message', 'Welcome!') // send to single user
     socket.broadcast.emit('message', 'A new user has joined') // send to everyone except user
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        // filter profanity
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed')
+        }
+
+        // if no profanity, send message
         io.emit('message', message) // send to all users
+        callback()
     })
 
-    socket.on('sendLocation', (coords) => {
+    socket.on('sendLocation', (coords, callback) => {
         io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback() // acknowledgement that location sent
     })
 
     socket.on('disconnect', () => {
