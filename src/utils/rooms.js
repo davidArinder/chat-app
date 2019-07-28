1767db3e4922050b7b1cdbbeb6a57596f4bcb1c6
David Arinder <david.arinder@gmail.com>
2019-07-28 13:34:01 -0700

    joinable rooms test

--- src/utils/rooms.js
+++ src/utils/rooms.js
@@ -1,10 +1,10 @@
 const rooms = []
 
 // add rooms to array
-const addRooms = (room) => {
+const addRooms = ({ room }) => {
     // check if room already exists
-    if (room) {
-        return
+    if (!room) {
+        return console.log('No room')
     // add room to array
     } else {
         rooms.push(room)
@@ -17,10 +17,23 @@ const getRooms = () => {
 
 }
 
-// remove rooms from array when all users leave
-const removeRooms = () => {
+const Seattle = addRooms({
+    room: 'Seattle',
+    members: 2
+})
+const Renton = addRooms({
+    room: 'Renton',
+    members: 0
+})
 
+// remove rooms from array when all users leave
+const removeRooms = (e) => {
+    console.log(Seattle[0], Renton[0])
 }
+
+removeRooms(rooms)
+
+console.log(rooms)
 
 module.exports = {
     addRooms
