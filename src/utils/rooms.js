c63c47b33a1152e152fc332ac5c5dfee3818c2fe
David Arinder <david.arinder@gmail.com>
2019-07-30 13:19:37 -0700

    adding joinable rooms

--- src/utils/rooms.js
+++ src/utils/rooms.js
@@ -1,7 +1,7 @@
 const rooms = []
 
 // add rooms to array
-const addRooms = ({ room }) => {
+const addRooms = (room) => {
     // check if room already exists
     if (!room) {
         return console.log('No room')
@@ -17,24 +17,36 @@ const getRooms = () => {
 
 }
 
-const Seattle = addRooms({
+// example rooms
+addRooms({
     room: 'Seattle',
     members: 2
 })
-const Renton = addRooms({
+addRooms({
     room: 'Renton',
     members: 0
 })
-
-// remove rooms from array when all users leave
-const removeRooms = (e) => {
-    console.log(Seattle[0], Renton[0])
-}
-
-removeRooms(rooms)
+addRooms({
+    room: 'Bellevue',
+    members: 10
+})
+addRooms({
+    room: 'Lynnwood',
+    members: 0
+})
 
 console.log(rooms)
 
+// remove rooms from array when all users leave
+const removeRooms = rooms.filter(room => room.members > 0);
+console.log(removeRooms)
+
+// removeRooms(rooms)
+
+
+// console.log(rooms[0].members)
+
 module.exports = {
-    addRooms
+    addRooms,
+    removeRooms
 }
